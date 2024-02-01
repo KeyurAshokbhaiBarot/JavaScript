136f99de2df2e70a268d5b8bd1feb158ea54220a
“KeyurAshokbhaiBarot” <link.keyur@gmail.com>
2024-02-01 15:31:31 -0600

    String operations and conversions

---
+++ 01_basics/operations.js
@@ -0,0 +1,25 @@
+let value = 3
+let negValue = -value
+
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // this is 2 to the power of 3
+// console.log(2/2);
+// console.log(2%2);
+
+let string1 = "Hello"
+let string2 = " Keyur"
+let string3 = string1 + string2
+console.log(string3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// if string is first then everything after that will be treated as a string,
+// if string is at the end, then first the conversion will take place and then string wil be added.
+
