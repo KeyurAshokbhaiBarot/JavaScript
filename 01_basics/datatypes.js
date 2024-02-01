35d5f3c66c8af31068855a7fa90af588dd301a89
“KeyurAshokbhaiBarot” <link.keyur@gmail.com>
2024-02-01 14:38:04 -0600

    datatypes and ECMA standards

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -3,3 +3,24 @@
 //alert(3 + 3) // we are using nodejs, not browser
 
 console.log(3+3)
+
+let name = "Keyur"
+let age = 18
+let isLoggedIn = false
+
+
+// primitive dataypes:
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined); // undefined
+
+console.log(typeof null); //object
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -3,9 +3,10 @@ let accountEmail = "keyur@gmail.com"
 var accountPassword = "Texas"
 accountCity = "Mumbai"
 let accounState
+let userName = "keyur"
 
 // console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, accountCity, accounState])
+console.table([accountId, accountEmail, accountPassword, accountCity, accounState, userName])
 
 /*
 prefer not to use var as there is an issue in block and functional scope
