64080d5807f19437e8d3ba6a3df80e7fee26ef05
“KeyurAshokbhaiBarot” <link.keyur@gmail.com>
2024-02-01 13:27:50 -0600

    variables in JS

---
+++ 01_basics/test.js
@@ -0,0 +1,12 @@
+const accountId = 1444553
+let accountEmail = "keyur@gmail.com"
+var accountPassword = "Texas"
+accountCity = "Mumbai"
+let accounState
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accounState])
+
+/*
+prefer not to use var as there is an issue in block and functional scope
+*/
